3415bb94084b76806ee198f2f152a7d2f41e2233
idempiere-consulting <info@idempiere.consulting>
2025-03-18 15:11:51 +0100

    aChecchia: plug-ins orderTemplate e groupware --- Su infoWindow 'Info Crea ODV/Opportunità/Calendario da Richiesta [ICOOC00]', modifiche varie per i processi di 'Crea Offerta di Vendita da Richiesta' e 'Crea Evento Calendario da Richiesta'

--- src/jpiere/plugin/groupware/window/ToDoPopupWindow.java
+++ src/jpiere/plugin/groupware/window/ToDoPopupWindow.java
@@ -3463,7 +3463,8 @@ public class ToDoPopupWindow extends Window implements EventListener<Event>,Valu
 			}
 			if(map_Editor.get(MToDo.COLUMNNAME_Description).getValue()==null || ((String)map_Editor.get(MToDo.COLUMNNAME_Description).getValue()).isEmpty())
 				map_Editor.get(MToDo.COLUMNNAME_Description).setValue(cTask.getDescription());
-			map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(cTask.get_ValueAsInt("C_BPartner_ID"));
+			if(cTask.get_ValueAsInt("C_BPartner_ID") > 0)
+				map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(cTask.get_ValueAsInt("C_BPartner_ID"));
 			
 		}
 		else if(MToDo.COLUMNNAME_C_Project_ID.equals(name)) {
@@ -3481,7 +3482,8 @@ public class ToDoPopupWindow extends Window implements EventListener<Event>,Valu
 			}
 			if(map_Editor.get(MToDo.COLUMNNAME_Description).getValue()==null || ((String)map_Editor.get(MToDo.COLUMNNAME_Description).getValue()).isEmpty())
 				map_Editor.get(MToDo.COLUMNNAME_Description).setValue(cProject.getDescription());
-			map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(cProject.get_ValueAsInt("C_BPartner_ID"));
+			if(cProject.get_ValueAsInt("C_BPartner_ID") > 0)
+				map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(cProject.get_ValueAsInt("C_BPartner_ID"));
 			
 		}
 		else if("MP_Maintain_ID".equals(name)) {
@@ -3499,7 +3501,8 @@ public class ToDoPopupWindow extends Window implements EventListener<Event>,Valu
 			}
 			if(map_Editor.get(MToDo.COLUMNNAME_Description).getValue()==null || ((String)map_Editor.get(MToDo.COLUMNNAME_Description).getValue()).isEmpty())
 				map_Editor.get(MToDo.COLUMNNAME_Description).setValue(mpMaintain.getDescription());
-			map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(mpMaintain.get_ValueAsInt("C_BPartner_ID"));
+			if(mpMaintain.get_ValueAsInt("C_BPartner_ID") > 0)
+				map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(mpMaintain.get_ValueAsInt("C_BPartner_ID"));
 			
 		}
 		else if("MP_OT_ID".equals(name)) {
@@ -3517,7 +3520,8 @@ public class ToDoPopupWindow extends Window implements EventListener<Event>,Valu
 			}
 			if(map_Editor.get(MToDo.COLUMNNAME_Description).getValue()==null || ((String)map_Editor.get(MToDo.COLUMNNAME_Description).getValue()).isEmpty())
 				map_Editor.get(MToDo.COLUMNNAME_Description).setValue(mpOT.getDescription());
-			map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(mpOT.get_ValueAsInt("C_BPartner_ID"));
+			if(mpOT.get_ValueAsInt("C_BPartner_ID") > 0)
+				map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(mpOT.get_ValueAsInt("C_BPartner_ID"));
 			
 		}
 		else if(MToDo.COLUMNNAME_R_Request_ID.equals(name)) {
@@ -3535,7 +3539,10 @@ public class ToDoPopupWindow extends Window implements EventListener<Event>,Valu
 			}
 			if(map_Editor.get(MToDo.COLUMNNAME_Description).getValue()==null || ((String)map_Editor.get(MToDo.COLUMNNAME_Description).getValue()).isEmpty())
 				map_Editor.get(MToDo.COLUMNNAME_Description).setValue(rRequest.getSummary());
-			map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(rRequest.get_ValueAsInt("C_BPartner_ID"));
+			if(rRequest.get_ValueAsInt("C_BPartner_ID") > 0)
+				map_Editor.get(MToDo.COLUMNNAME_C_BPartner_ID).setValue(rRequest.get_ValueAsInt("C_BPartner_ID"));
+			if(rRequest.getQtyPlan()!=null && rRequest.getQtyPlan().compareTo(BigDecimal.ZERO) > 0)
+				((WNumberEditor)map_Editor.get(MToDo.COLUMNNAME_Qty)).setValue(rRequest.getQtyPlan());
 			
 		}
 		//iDempiereConsulting __26/10/2021 -------END
@@ -3664,6 +3671,12 @@ public class ToDoPopupWindow extends Window implements EventListener<Event>,Valu
 				map_Editor.get(MToDo.COLUMNNAME_C_Project_ID).setValue(projectID);
 				p_c_project_ID = projectID;
 				((WSearchEditor)map_Editor.get(MToDo.COLUMNNAME_C_Project_ID)).valueChange(new ValueChangeEvent(map_Editor.get(MToDo.COLUMNNAME_C_Project_ID), "Change", null, projectID));
+			}
+			BigDecimal qty =  (BigDecimal)map_Editor.get(MToDo.COLUMNNAME_Qty).getValue();
+			if(qty != null && qty.compareTo(BigDecimal.ZERO) > 0) {
+				LocalDateTime start = ((Timestamp)((WTimeEditor)map_Editor.get(MToDo.COLUMNNAME_JP_ToDo_ScheduledStartTime)).getValue()).toLocalDateTime();
+				LocalDateTime end = start.plusHours(qty.longValue());
+				map_Editor.get(MToDo.COLUMNNAME_JP_ToDo_ScheduledEndTime).setValue(Timestamp.valueOf(end));
 			}
 		}
 	}
